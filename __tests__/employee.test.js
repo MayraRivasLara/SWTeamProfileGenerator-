18e07c32e5ebe26260e7a09e05a7577a39ab32dd
Mayra Rivas Lara <mayrulara@hotmail.com>
2022-02-20 21:05:33 +0800

    second commit'

---
+++ __tests__/employee.test.js
@@ -0,0 +1,25 @@
+const Employee = require('./../src/employee/employee/');
+
+// section
+
+describe('Employee class test', ()  => {
+
+    // it can be instantiated
+    
+    // take in the "name" argument and set it as property.
+
+    //take in the "id" argument and set it as property.
+
+    //take in the "email" argument and set it as property.
+
+    it('can get the employee name by running the getName()')
+
+    // it function to run the test.
+
+})
+
+// test getEmail()
+
+// test getRole()
+
+// test getId()
